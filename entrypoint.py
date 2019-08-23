0b9d5775a7c888b93c4ee78b859853de7bc2ba96
Jamie Bliss <jamie@ivyleav.es>
2019-08-23 15:08:16 -0400

    Initial commit

---
+++ entrypoint.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import os
+import sys
+import shutil
+
+
+print("== ARGV ==")
+
+for arg in sys.argv:
+    print(f"{len(arg)}\t{arg}")
+
+
+print("")
+print("== ENVIRON ==")
+for k, v in os.environ.items():
+    print(f"{k}: {v}")
+
+print("")
+print("== STDIN ==")
+shutil.copyfileobj(sys.stdin, sys.stdout)
+sys.stdout.flush()
