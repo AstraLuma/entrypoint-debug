9bec564cfa9d621deb9539707daf269adfab8148
Jamie Bliss <jamie@ivyleav.es>
2019-08-23 15:50:20 -0400

    Add option to read files specified in argv

--- entrypoint.py
+++ entrypoint.py
@@ -15,7 +15,18 @@ print("== ENVIRON ==")
 for k, v in os.environ.items():
     print(f"{k}:\t{v}")
 
+
+for arg in sys.argv[1:]:
+    if os.path.isfile(arg):
+        print("")
+        print(f"== FILE: {arg} ==")
+        with open(arg, 'r') as fo:
+            shutil.copyfileobj(fo, sys.stdout)
+
+
 print("")
 print("== STDIN ==")
 shutil.copyfileobj(sys.stdin, sys.stdout)
+
+
 sys.stdout.flush()
